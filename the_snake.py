8c7e27399ffc30aa9655cc09b315d0e49f98af6e
Nikita Zakharov <zaakniki@gmail.com>
2026-08-15 19:15:34 +0300

    Исправлен код в соответствии с PEP8

--- the_snake.py
+++ the_snake.py
@@ -1,18 +1,23 @@
-from random import choice, randint
+from random import randint
 
 import pygame
+
 
 SCREEN_WIDTH: int = 640
 SCREEN_HEIGHT: int = 480
 CENTER_POSITION: tuple = (SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2)
+
+
 GRID_SIZE: int = 20
 GRID_WIDTH: int = SCREEN_WIDTH // GRID_SIZE
 GRID_HEIGHT: int = SCREEN_HEIGHT // GRID_SIZE
+
 
 UP: tuple = (0, -1)
 DOWN: tuple = (0, 1)
 LEFT: tuple = (-1, 0)
 RIGHT: tuple = (1, 0)
+
 
 BOARD_BACKGROUND_COLOR: tuple = (0, 0, 0)
 BORDER_COLOR: tuple = (93, 216, 228)
@@ -21,6 +26,7 @@ SNAKE_COLOR: tuple = (0, 255, 0)
 
 SPEED: int = 20
 
+
 DIRECTION_MAP: dict = {
     (pygame.K_UP, DOWN): UP,
     (pygame.K_DOWN, UP): DOWN,
@@ -28,13 +34,11 @@ DIRECTION_MAP: dict = {
     (pygame.K_RIGHT, LEFT): RIGHT,
 }
 
+
 screen: pygame.Surface = pygame.display.set_mode(
     (SCREEN_WIDTH, SCREEN_HEIGHT), 0, 32
 )
-
-
 pygame.display.set_caption('Змейка')
-
 
 clock: pygame.time.Clock = pygame.time.Clock()
 
@@ -68,7 +72,7 @@ class Apple(GameObject):
         self.randomize_position()
 
     def randomize_position(
-            self, 
+            self,
             occupied_positions: list | None = None
     ) -> None:
         """Устанавливает случайное положение яблока на игровом поле."""
@@ -142,7 +146,7 @@ def handle_keys(game_object: Snake) -> None:
         if event.type == pygame.QUIT:
             pygame.quit()
             raise SystemExit
-        elif event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             new_direction = DIRECTION_MAP.get(
                 (event.key, game_object.direction)
             )
@@ -174,3 +178,7 @@ def main() -> None:
         snake.draw()
         apple.draw()
         pygame.display.update()
+
+
+if __name__ == "__main__":
+    main()
